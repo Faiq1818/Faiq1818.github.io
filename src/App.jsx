fab859a1e18cceee1b0878a7d39609544564cf6b
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-05-27 01:07:48 +0700

    making web base

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={styles.container}>
+      <div style={styles.containermid}>
+        <div>
+          <p style={styles.texts_name}>Faiq Ghozy Erlangga</p>
+        </div>
+        <div>
+          <p style={styles.texts}>halo</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+
+const styles = {
+  texts_name: {
+    color: "#2C3930",
+    alignItems: "",
+    // padding: "20px"
+  },
+
+  texts: {
+    color: "black",
+    alignItems: "black",
+    // padding: "20px"
+  },
+
+  container: {
+    height: "100vh",
+    width: "100vw",
+    overflow: "hidden",
+    border: "30px solid #DCD7C9",
+    boxShadow: "inset 0 0 0 5px #A27B5C",
+    display: "flex",
+    justifyContent: "center",
+    // alignItems: "center",
+    backgroundColor: "#DCD7C9",
+    boxSizing: "border-box",
+  },
+
+  containermid: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%"
+  }
+}
+
+export default App; 
