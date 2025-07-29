02f7c1d907d8df9c5e614e011e1296ea11f5d5aa
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-30 02:06:45 +0700

    .

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex h-screen w-screen overflow-hidden bg-[#000000]" style={{ border: "30px solid #000000", boxShadow: "inset 0 0 0 1px #ffffff"}}>
+        <div>
+          <div>
+            {/* <p className="text-5xl font-semibold">Faiq Ghozy Erlangga</p> */}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
