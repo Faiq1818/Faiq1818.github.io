93cb714294e0a23292ed5f148f3d7b274da3029b
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-30 14:42:21 +0700

    adding ubuntu font

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen w-screen overflow-hidden bg-[#000000]" style={{ border: "30px solid #000000", boxShadow: "inset 0 0 0 1px #ffffff"}}>
+      <div className="flex h-screen w-screen overflow-hidden bg-[#000000] p-6" style={{ border: "30px solid #000000", boxShadow: "inset 0 0 0 0.2px #ffffff"}}>
         <div>
           <div>
-            {/* <p className="text-5xl font-semibold">Faiq Ghozy Erlangga</p> */}
+            <p className="text-5xl font-semibold text-[#c4c4c4]">Faiq Ghozy Erlangga</p>
           </div>
         </div>
       </div>
