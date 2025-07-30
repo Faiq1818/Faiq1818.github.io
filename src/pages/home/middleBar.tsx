3dc46cd48e5bff4b183fa6ebd56e15ff8f11ee0e
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-30 19:51:38 +0700

    adding theme swithc

--- src/pages/home/middleBar.tsx
+++ src/pages/home/middleBar.tsx
@@ -1,9 +1,40 @@
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { FaProjectDiagram } from "react-icons/fa";
+import { FaPerson } from "react-icons/fa6";
+import { BiSolidContact } from "react-icons/bi";
+
 export default function MiddleBar() {
   return (
-    <div className="mt-auto flex flex-col">
-      <p className="text-xl my-5 text-[#c4c4c4] font-medium">+ About me</p>
-      <p className="text-xl my-5 text-[#c4c4c4] font-medium">+ Project</p>
-      <p className="text-xl my-5 text-[#c4c4c4] font-medium">+ Contact</p>
+    <div className="mt-auto mx-5 text-xl flex flex-col">
+      <div className="flex flex-row items-center">
+        <FaPerson className="text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer" />
+        <p className="my-5 text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer mx-2">
+          About me
+        </p>
+      </div>
+
+      <div className="flex flex-row items-center">
+        <FaProjectDiagram className="text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer" />
+
+        <Popover className="text-white relative mx-2">
+          <PopoverButton className="text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer">
+            Project
+          </PopoverButton>
+          <PopoverPanel anchor="right" className="flex flex-col bg-white">
+            <a href="/analytics">Analytics</a>
+            <a href="/engagement">Engagement</a>
+            <a href="/security">Security</a>
+            <a href="/integrations">Integrations</a>
+          </PopoverPanel>
+        </Popover>
+      </div>
+
+      <div className="flex flex-row items-center">
+        <BiSolidContact className="text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer" />
+        <p className="my-5 text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer mx-2">
+          Contact
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/home/topSection.tsx
+++ src/pages/home/topSection.tsx
@@ -1,48 +1,60 @@
-import {
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { useState } from "react";
 import { IoLanguage } from "react-icons/io5";
+import { MdDarkMode } from "react-icons/md";
+import { Switch } from "@headlessui/react";
 
 export default function TopSection() {
   let [isOpen, setIsOpen] = useState(false);
+  const [enabled, setEnabled] = useState(false);
+
   return (
     <div className="flex flex-row justify-between">
       <p className="text-4xl font-semibold text-[#c4c4c4]">
         Faiq Ghozy Erlangga
       </p>
-      <div
-        className="flex flex-row items-center text-[#818181] hover:text-[#BABABA]"
-        onClick={() => setIsOpen(true)}
-      >
-        <IoLanguage size={"1.2em"} />
-        <p className="ml-2 pr-3">English</p>
+      <div className="flex flex-row">
+        <div className="flex cursor-pointer flex-row items-center text-[#818181] hover:text-[#BABABA]">
+          <MdDarkMode />
+          <Switch
+            checked={enabled}
+            onChange={setEnabled}
+            className="group ml-2 inline-flex h-5 w-10 items-center rounded-full bg-[#444444] transition data-checked:bg-[#818181]"
+          >
+            <span className="size-3 translate-x-1 rounded-full bg-[#c4c4c4] transition group-data-checked:translate-x-6" />
+          </Switch>
+        </div>
+        <div className="flex cursor-pointer flex-row items-center text-[#818181] hover:text-[#BABABA] ml-5" onClick={() => setIsOpen(true)}>
+          <IoLanguage size={"1.2em"} />
+          <p className="ml-2 pr-3">English</p>
+        </div>
       </div>
+
       <>
         <Dialog
           open={isOpen}
           onClose={() => setIsOpen(false)}
           transition
-          className="fixed inset-0 flex w-screen items-center justify-center z-50 bg-black/30 p-4 transition duration-300 ease-out data-closed:opacity-0"
+          className="fixed inset-0 flex w-screen items-center justify-center z-50 bg-black/45 p-4 transition duration-300 ease-out data-closed:opacity-0"
         >
           <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
             <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-              <DialogTitle className="font-bold">
-                Deactivate account
+              <DialogTitle className="font-bold items-center">
+                Choose a language
               </DialogTitle>
-              <Description>
-                This will permanently deactivate your account
-              </Description>
-              <p>
-                Are you sure you want to deactivate your account? All of your
-                data will be permanently removed.
-              </p>
               <div className="flex gap-4">
-                <button onClick={() => setIsOpen(false)}>Cancel</button>
-                <button onClick={() => setIsOpen(false)}>Deactivate</button>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => setIsOpen(false)}
+                >
+                  English
+                </button>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Bahasa Indonesia
+                </button>
               </div>
             </DialogPanel>
           </div>
