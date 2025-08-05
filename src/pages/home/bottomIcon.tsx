9e836ef8142ed5424e476972b43823d23230246c
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-08-05 15:26:57 +0700

    adding contact me panel and adding link in social media icon in bottom section in home

--- src/pages/home/bottomIcon.tsx
+++ src/pages/home/bottomIcon.tsx
@@ -5,18 +5,24 @@ import { FaInstagram } from "react-icons/fa";
 export default function BottomIcon() {
   return (
     <div className="mt-auto flex justify-center text-[#818181]">
-      <FaGithub
-        size={"1.2em"}
-        className="mx-2 hover:text-[#BABABA] cursor-pointer"
-      />
-      <FaLinkedin
-        size={"1.2em"}
-        className="mx-2 hover:text-[#BABABA] cursor-pointer"
-      />
-      <FaInstagram
-        size={"1.2em"}
-        className="mx-2 hover:text-[#BABABA] cursor-pointer"
-      />
+      <a href="https://github.com/Faiq1818">
+        <FaGithub
+          size={"1.2em"}
+          className="mx-2 hover:text-[#BABABA] cursor-pointer"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/faiq-ghozy-erlangga-378b19321/">
+        <FaLinkedin
+          size={"1.2em"}
+          className="mx-2 hover:text-[#BABABA] cursor-pointer"
+        />
+      </a>
+      <a href="https://www.instagram.com/faiqghozyerlangga/">
+        <FaInstagram
+          size={"1.2em"}
+          className="mx-2 hover:text-[#BABABA] cursor-pointer"
+        />
+      </a>
     </div>
   );
 }
--- src/pages/home/middleBar.tsx
+++ src/pages/home/middleBar.tsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router";
 import { FaProjectDiagram } from "react-icons/fa";
 import { FaPerson } from "react-icons/fa6";
+import { useState } from 'react'
 import { BiSolidContact } from "react-icons/bi";
+import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 
 export default function MiddleBar() {
+  let [isOpen, setIsOpen] = useState(false)
   const navigate = useNavigate();
 
   const goToAbout = () => {
@@ -13,6 +16,14 @@ export default function MiddleBar() {
   const goToProject = () => {
     navigate('/project');
   };
+
+  function open() {
+    setIsOpen(true)
+  }
+
+  function close() {
+    setIsOpen(false)
+  }
 
   return (
     <div className="flex mt-auto flex-row justify-between">
@@ -33,11 +44,45 @@ export default function MiddleBar() {
 
         <div className="flex flex-row items-center">
           <BiSolidContact className="text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer" />
-          <p className="my-5 text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer mx-2">
-            Contact
+          <p onClick={open} className="my-5 text-[#c4c4c4] hover:text-[#E4E4E4] cursor-pointer mx-2">
+            Contact me
           </p>
         </div>
       </div>
+
+      <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close}>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            >
+              <div className="flex flex-row justify-between">
+                <DialogTitle as="h3" className="text-base/7 font-medium text-white">
+                  Contact me
+                </DialogTitle>
+                <Button
+                  className="inline-flex items-center gap-2 rounded-full px-2 py-1 text-sm font-semibold text-white shadow-white/10 focus:not-data-focus:outline-none data-focus:outline  data-hover:text-gray-400"
+                  onClick={close}
+                >
+                  x
+                </Button>
+              </div>
+              <p className="mt-2 text-sm/6 text-white/50">
+                You can contact me by sending me an email or reach me using my social media.
+              </p>
+              <div className="mt-2 text-sm/6 text-white/80">
+                ghozyerlanggafaiq@gmail.com
+              </div>
+              <div className="mt-2 text-sm/6 text-white/80">
+                My Instagram
+              </div>
+              <div className="mt-4">
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
    </div>
   );
 }
